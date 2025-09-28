83bfb52ae9a27f3976565acd49fc260f020f2035
NightFuryL <levodesskiy@gmail.com>
2025-09-28 16:46:45 +0300

    the second Practice 28.09.2025

--- lessonPractice.cpp
+++ lessonPractice.cpp
@@ -1,74 +1,67 @@
 #include <iostream>
 #include <windows.h>
 #include <string>
-#include <vector>
 using namespace std;
 
-class Passport {
+class Student {
 protected:
     string firstName;
     string lastName;
-    string nationality;
-    string dateOfBirth;
-    string passportNumber;
-    string expiryDate;
-    string sex;//Стать !
+    string group;
+    double averageGrade;
 
 public:
-    Passport() {}
-
-    Passport(string firstName, string lastName, string nation, string dateOfBirth, string passportNumber, string expiryDate, string sex) : firstName(firstName), lastName(lastName), nationality(nation), dateOfBirth(dateOfBirth), passportNumber(passportNumber), expiryDate(expiryDate), sex(sex) {
+    Student() {}
+    Student(string firstName, string lastName, string group, double avgGrade) : firstName(firstName), lastName(lastName), group(group), averageGrade(avgGrade) {
     }
+
     void showInfo() {
         cout << "First Name : " << firstName << endl;
         cout << "Last Name : " << lastName << endl;
-        cout << "Nationality : " << nationality << endl;
-        cout << "Date of Birth : " << dateOfBirth << endl;
-        cout << "Passport Number : " << passportNumber << endl;
-        cout << "Expiry Date : " << expiryDate << endl;
-        cout << "Sex : " << sex << endl;
+        cout << "Group : " << group << endl;
+        cout << "Average Grade : " << averageGrade << endl;
     }
-
 };
 
-class ForeignPassport : public Passport {
+class Aspirant : public Student {
 private:
-    string foreignPassNumber;
-	string foreignExpiryDate;
-    vector<string> visas;
+    string thesisTopic;
+    string text;
 
 public:
-    ForeignPassport() {}
+    Aspirant() {}
 
-    ForeignPassport(string firstName, string lastName, string nation, string dateOfBirth,string passportNumber, string expiryDateDefaultPassport,string foreignNum,string foreignExpiryDate,string sex) : Passport(firstName, lastName, nation, dateOfBirth, passportNumber, expiryDateDefaultPassport, sex), foreignPassNumber(foreignNum), foreignExpiryDate(foreignExpiryDate) {
+    Aspirant(string firstName, string lastName, string group, double avgGrade, string topic, string text) : Student(firstName, lastName, group, avgGrade), thesisTopic(topic), text(text) {
     }
 
-    void addVisa(const string& visa) {
-        visas.push_back(visa);
-    }
-
-    void showForeignInfo() {
-        showInfo();
-        cout << "Foreign Passport Number : " << foreignPassNumber << endl;
-		cout << "Foreign Passport Expiry Date : " << foreignExpiryDate << endl;
-        cout << "Visas: ";
-        if (visas.empty()) cout << "None" << endl;
-        else {
-            cout << endl;
-            for (int i = 0; i < visas.size(); ++i) {
-                cout << i <<  " - " << visas[i] << endl;
-            }
-        }
+	void protectionCandidateTheme() {
+        //showInfo();
+		cout << "-----------------------------------" << endl;
+        cout << "Thesis Topic: " << thesisTopic << endl;
+		cout << "Aspirant is defending the thesis topic." << endl;
+		cout << "-----------------------------------" << endl;
+        cout << "\tAspirant : " << firstName << " " << lastName << endl
+             << "\tGroup : " << group << endl
+             << "\tAverage grade : " << averageGrade << endl
+			 << "\tDefending the thesis topic : " << thesisTopic << endl;
+		cout << "-----------------------------------" << endl;
+		//cout << "bla bla bla..." << endl;
+		cout << text << endl;
+		cout << "-----------------------------------" << endl;
+		cout << "Thesis defense completed." << endl;
+		cout << "-----------------------------------" << endl;
     }
 };
 
 int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-    ForeignPassport fp("Лев", "Богля", "Україна", "11.11.1111", "UA123456789", "22.22.2222", "FOREIGN123456789", "30.30.3030", "Male");
-    fp.addVisa("USA");
-    fp.addVisa("Canada");
-    fp.showForeignInfo();
-
+    Aspirant asp("Lev", "Bohlia", "P410", 5, "Space Exploration", "asdfgk;ljihguytdfugihojpk[l");
+	asp.showInfo();
+    for (int i = 0; i < 5; i++)
+    {
+        cout << endl;
+    }
+    asp.protectionCandidateTheme();
     return 0;
 }
